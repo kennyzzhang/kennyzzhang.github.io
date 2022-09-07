ebabe9a81a6a4fcaa53d96b1a1e29d399b7071a9
kennyzzhang <kennyzzhang@gmail.com>
2022-09-06 22:22:10 -0400

    include head fix, ellipsis notation

--- scripts/includes.js
+++ scripts/includes.js
@@ -1,14 +1,14 @@
 window.addEventListener('DOMContentLoaded', (e) => {
   let parser = new DOMParser();
-  Array.from(document.getElementsByClassName('include')).map(async elt => {
-    let classes = Array.from(elt.classList.values());
+  [...document.getElementsByClassName('include')].map(async elt => {
+    let classes = [...elt.classList.values()];
     let include_class_name = classes.find(s => s.startsWith('include_'));
     let include_name = include_class_name.replace(/^include_/, '');
     let include_file_name = include_name + '.html';
     let include_response = await fetch(include_file_name);
     let include_text = await include_response.text();
     let include_doc = parser.parseFromString(include_text, 'text/html');
-    include_doc.head.childNodes.forEach(document.head.appendChild);
+    document.head.append(...include_doc.head.children);
     let include_elt = include_doc.getElementById(include_name);
     include_elt.removeAttribute('id');
     include_elt.classList.add(include_class_name);
