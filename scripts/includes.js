635ca4f1bf2060b7fde72613f1da8fa38fb0bd12
kennyzzhang <kennyzzhang@gmail.com>
2022-09-06 22:08:26 -0400

    fetch text promise fix

--- scripts/includes.js
+++ scripts/includes.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
     let include_name = include_class_name.replace(/^include_/, '');
     let include_file_name = include_name + '.html';
     let include_response = await fetch(include_file_name);
-    let include_text = include_response.text();
+    let include_text = await include_response.text();
     let include_doc = parser.parseFromString(include_text, 'text/html');
     let include_elt = include_doc.getElementById(include_name);
     include_elt.removeAttribute('id');
