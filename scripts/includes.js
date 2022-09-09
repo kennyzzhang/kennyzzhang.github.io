f483e00fd469c8d01c4d7556fd47d37f2fe30049
kennyzzhang <kennyzzhang@gmail.com>
2022-09-08 21:01:52 -0400

    include script test

--- scripts/includes.js
+++ scripts/includes.js
@@ -21,7 +21,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
     include_elt.classList.add(include_name);
     head_elts = [...include_doc.head.children];
     head_elts_loaded = Promise.all(head_elts.map(head_elt => new Promise((resolve, reject) => {
-      head_elt.addEventListener('load', (evt) => {resolve()});
+      head_elt.addEventListener('load', (evt) => {console.log(evt); console.log(head_elt); resolve()});
     }))).then(() => {
       elt.replaceWith(include_elt);
     });
