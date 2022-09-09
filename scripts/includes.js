785ced55f874fda268c5924a5be098ea59deca6d
kennyzzhang <kennyzzhang@gmail.com>
2022-09-08 20:54:33 -0400

    error handling in include script

--- scripts/includes.js
+++ scripts/includes.js
@@ -6,15 +6,25 @@ window.addEventListener('DOMContentLoaded', (e) => {
     let include_name = include_class_name.replace(/^include_/, '');
     let include_file_name = include_name + '.html';
     let include_response = await fetch(include_file_name);
+    if (!include_response.ok) {
+      console.warn(`Failed to fetch ${include_file_name}`);
+      return;
+    }
     let include_text = await include_response.text();
     let include_doc = parser.parseFromString(include_text, 'text/html');
-    for (let child of include_doc.head.children) {
-      child.addEventListener('load', (evt) => {console.log(evt); console.log(child)});
-    }
-    document.head.append(...include_doc.head.children);
     let include_elt = include_doc.getElementById(include_name);
+    if (include_elt === null) {
+      console.warn(`Failed to find include element for ${include_name}`);
+      return;
+    }
     include_elt.removeAttribute('id');
     include_elt.classList.add(include_name);
-    elt.replaceWith(include_elt);
+    head_elts = [...include_doc.head.children];
+    head_elts_loaded = Promise.all(head_elts.map(head_elt => new Promise((resolve, reject) => {
+      head_elt.addEventListener('load', (evt) => {resolve()});
+    }))).then(() => {
+      elt.replaceWith(include_elt);
+    });
+    document.head.append(...head_elts);
   });
 });
